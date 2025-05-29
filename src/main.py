5cf5ebe7761d0dc7afadbc5721be5afa83d9a263
JohnEbert <johnthomas.ebert@gmail.com>
2025-05-29 19:30:22 +0200

    Main Flask app added, html templates added

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+main = app
+if __name__ == "__main__":
+    app.run()
+    # This line is not needed in production, but useful for debugging
+    
+        
