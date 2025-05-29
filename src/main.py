546f62f5373e1e6f1ee438dff843c769d1db3407
JohnEbert <johnthomas.ebert@gmail.com>
2025-05-29 21:36:58 +0200

    Add login and scheduler pages with corresponding routes and templates

--- src/main.py
+++ src/main.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/scheduler', methods=['GET', 'POST'])
+def scheduler():
+    return render_template("scheduler.html")
+
 
 main = app
 if __name__ == "__main__":
